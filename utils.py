29b6e9768e5df10c575d29196aca44daf2205944
williamcrain <william.crain@capitalone.com>
2017-03-16 07:37:30 -0400

    added imports

--- utils.py
+++ utils.py
@@ -1,4 +1,33 @@
+import argparse
+import codecs
+import csv
+import json
+import operator
+import os
+import os.path
+import sys
+import re
+import boto3
+import botocore.exceptions
 
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+try:
+    import yaml
+    NO_YAML = False
+except ImportError:
+    NO_YAML = True
+
+from base64 import b64encode, b64decode
+from boto3.dynamodb.conditions import Attr
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.hmac import HMAC
 
 def clean_fail(func):
     '''
