69d97f3be6f71ff6cfaea294a9c6ac2c29ab0dd3
williamcrain <william.crain@capitalone.com>
2017-03-16 07:42:06 -0400

    moved clean fail

--- dynamodb.py
+++ dynamodb.py
@@ -1,5 +1,20 @@
 from utils import *
 
+def clean_fail(func):
+    '''
+    A decorator to cleanly exit on a failed call to AWS.
+    catch a `botocore.exceptions.ClientError` raised from an action.
+    This sort of error is raised if you are targeting a region that
+    isn't set up (see, `credstash setup`.
+    '''
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except botocore.exceptions.ClientError as e:
+            print(str(e), file=sys.stderr)
+            sys.exit(1)
+    return func_wrapper
+
 @clean_fail
 def deleteSecrets(name, region=None, table="credential-store",
                   **kwargs):
