c009ad42d72d53fba8ab65aace49962223b40467
williamcrain <william.crain@capitalone.com>
2017-03-16 19:50:34 -0400

    fix unused var

--- credstash.py
+++ credstash.py
@@ -398,7 +398,7 @@ def main():
                 createDdbTable(region=region, table=args.table,
                                 **session_params)
             elif datastore == "s3":
-                createS3Credstash(region=region, bucket=args.bucket, s3credstash=s3credstash
+                createS3Credstash(region=region, bucket=args.bucket
                                 **session_params)
             else:
                 print(datastore + " is not a supported datastore")
