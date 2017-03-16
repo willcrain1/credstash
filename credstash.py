583bc9804cafae4e6a6001ee37ef1860060ba7aa
williamcrain <william.crain@capitalone.com>
2017-03-16 07:45:29 -0400

    moved back to utils

--- credstash.py
+++ credstash.py
@@ -66,20 +66,7 @@ PAD_LEN = 19  # number of digits in sys.maxint
 WILDCARD_CHAR = "*"
 
 
-def clean_fail(func):
-    '''
-    A decorator to cleanly exit on a failed call to AWS.
-    catch a `botocore.exceptions.ClientError` raised from an action.
-    This sort of error is raised if you are targeting a region that
-    isn't set up (see, `credstash setup`.
-    '''
-    def func_wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except botocore.exceptions.ClientError as e:
-            print(str(e), file=sys.stderr)
-            sys.exit(1)
-    return func_wrapper
+
 
 
 def printStdErr(s):
--- utils.py
+++ utils.py
@@ -1,31 +1,16 @@
-import argparse
-import codecs
-import csv
-import json
-import operator
-import os
-import os.path
-import sys
-import re
-import boto3
-import botocore.exceptions
+from __future__ import print_function
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-
-try:
-    import yaml
-    NO_YAML = False
-except ImportError:
-    NO_YAML = True
-
-from base64 import b64encode, b64decode
-from boto3.dynamodb.conditions import Attr
-
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives.hmac import HMAC
-
+def clean_fail(func):
+    '''
+    A decorator to cleanly exit on a failed call to AWS.
+    catch a `botocore.exceptions.ClientError` raised from an action.
+    This sort of error is raised if you are targeting a region that
+    isn't set up (see, `credstash setup`.
+    '''
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except botocore.exceptions.ClientError as e:
+            print(str(e), file=sys.stderr)
+            sys.exit(1)
+    return func_wrapper
